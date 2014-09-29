6921dc86bf631ac4376e496279fb799cad9a3b40
John L. Cooper <jlcooper@email.wm.edu>
2014-09-29 17:15:08 -0400

    some changes. not working.

--- nachos1/threads/grouptest.cc
+++ nachos1/threads/grouptest.cc
@@ -5,55 +5,48 @@ Condition *full;
 Condition *empty;
 Lock *pLock;
 Lock *cLock;
-int prodBuff, helloBuff, conBuff, BUFFERSIZE = 5;
+int prodBuff,conBuff, inBuffer = 0, BUFFERSIZE = 2;
 int loopCount, numLoops = 200;
 char * buff;
 char * hello;
+char strConsumer[12];
+char strProducer[12];
 
 void consumerThread(int threadNum) {
   DEBUG('t', "in consumer\n");
-  
-  while (1) {
+  char ch = 'a';
+  while (ch != '\0') {
     cLock->Acquire();
-    
-    while (conBuff == prodBuff) empty->Wait(cLock);
-    char ch = buff[conBuff];
+    if (inBuffer == 0) empty->Wait(cLock);
+    ch = buff[conBuff];
+    inBuffer--;
     conBuff = (conBuff + 1) % BUFFERSIZE;
-    // only to end the infinite loop after numLoops times
-    if (conBuff == 0) {
-      loopCount++;
-    }
-    // 
     
-    //if (conBuff == ((prodBuff + 1) % bufferSize))
-      full->Signal(pLock);
+    full->Signal(pLock);
     cLock->Release();
-
-    //printf("Thread %d output '%c'\n", threadNum, ch);
     printf("%c", ch);
-    //    if (ch == 'd') printf("\n");
-    if (loopCount >= numLoops) break;
   }
+  printf("%d\n", inBuffer);
+
 }
   
 void producerThread(int threadNum) {
   DEBUG('t', "in producer\n");
-  while (1){
+  int helloBuff = 0;
+  while (hello[helloBuff] != '\0') {
     pLock->Acquire();
-
-    while (((prodBuff + 1) % BUFFERSIZE) == conBuff) full->Wait(pLock);
+    if (inBuffer == BUFFERSIZE) full->Wait(pLock);
     buff[prodBuff] = hello[helloBuff];
-    //printf("Thread %d input %c\n", threadNum, buff[prodBuff]);
+    inBuffer++;
     prodBuff = (prodBuff + 1) % BUFFERSIZE;
-    helloBuff = (helloBuff + 1) % strlen(hello);
+    //helloBuff = (helloBuff + 1) % strlen(hello);
+    helloBuff++;
+    empty->Signal(cLock);
+    
 
-    //if (((prodBuff + 2) % bufferSize) == conBuff) 
-      empty->Signal(cLock);
     pLock->Release();
-    if (loopCount >= numLoops) break;
-
   }
-  
+  prodBuff = (prodBuff + 1) % BUFFERSIZE;
 }
   
 void lockTestStart() {
@@ -64,13 +57,11 @@ void lockTestStart() {
   cLock = new(std::nothrow) Lock("cLock");
 
   prodBuff = 0;
-  helloBuff = 0;
   conBuff = 0;
   loopCount = 0;
-  int numThreads = 5;
+  int numThreads = 1;
 
   hello = (char *)"Hello world";
-  //printf("%d\n", strlen(hello));
   buff = new char[BUFFERSIZE];
 
   Thread * consumer[numThreads];
@@ -80,8 +71,9 @@ void lockTestStart() {
 
   //create producer consumer
   for (i = 1; i <= numThreads; i++ ) {
-    char * strConsumer = (char *)"consumer";
-    char * strProducer = (char *)"producer";
+
+    sprintf(strConsumer, "%s%d", "consumer", i);
+    sprintf(strProducer, "%s%d", "producer", i);
 
     consumer[i] = new(std::nothrow) Thread(strConsumer);
     producer[i] = new(std::nothrow) Thread(strProducer);
