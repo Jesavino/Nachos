aaea1d001bdf7308f10bbb737f6569a313c9269e
John L. Cooper <jlcooper@email.wm.edu>
2014-10-27 07:56:12 -0400

    added test files for nachos 2

---
+++ nachos2/test/parent.c
@@ -0,0 +1,82 @@
+/* parent.c
+ *
+ * Parent in simple parent/child system.
+ *
+ */
+
+#include "syscall.h"
+
+int
+main()
+{
+
+  SpaceId kid;
+  int joinval;
+
+  prints("PARENT exists\n", ConsoleOutput);
+  kid = Exec("kid");
+  prints("PARENT after exec; kid pid is ", ConsoleOutput);
+  printd((int)kid, ConsoleOutput);
+  prints("\n", ConsoleOutput);
+
+  prints("PARENT about to Join kid\n", ConsoleOutput);
+  joinval = Join(kid);
+  prints("PARENT off Join with value of ", ConsoleOutput);
+  printd(joinval, ConsoleOutput);
+  prints("\n", 1, ConsoleOutput);
+
+  Halt();
+  /* not reached */
+}
+
+/* Print a null-terminated string "s" on open file descriptor "file". */
+
+prints(s,file)
+char *s;
+OpenFileId file;
+
+{
+  int count = 0;
+  char *p;
+
+  p = s;
+  while (*p++ != '\0') count++;
+  Write(s, count, file);  
+
+}
+
+/* Print an integer "n" on open file descriptor "file". */
+
+printd(n,file)
+int n;
+OpenFileId file;
+
+{
+
+  int i, pos=0, divisor=1000000000, d, zflag=1;
+  char c;
+  char buffer[11];
+  
+  if (n < 0) {
+    buffer[pos++] = '-';
+    n = -n;
+  }
+  
+  if (n == 0) {
+    Write("0",1,file);
+    return;
+  }
+
+  for (i=0; i<10; i++) {
+    d = n / divisor; n = n % divisor;
+    if (d == 0) {
+      if (!zflag) buffer[pos++] =  (char) (d % 10) + '0';
+    } else {
+      zflag = 0;
+      buffer[pos++] =  (char) (d % 10) + '0';
+    }
+    divisor = divisor/10;
+  }
+  Write(buffer,pos,file);
+}
+
